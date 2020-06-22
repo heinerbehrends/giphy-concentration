9c069f6fa4672f933a5c6b57ad0b5914a238f38e
heinerbehrends <heinerbehrends@hotmail.com>
2020-06-22 15:50:43 +0200

    Updated to new giphy client

--- src/Game.js
+++ src/Game.js
@@ -1,46 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import Board from './Board';
-import Search from './Search'
-import GphSdkClient from 'giphy-js-sdk-core'
-import { omgLinks } from './imageLinks';
+import Search from './Search';
+import { GiphyFetch } from '@giphy/js-fetch-api';
+
 import {
-  makeCards, makeNextCards, cardCount, flipCard, shouldFlip
+  makeCards,
+  makeNextCards,
+  cardCount,
+  flipCard,
+  shouldFlip,
 } from './gameHelpers';
 
+const gf = new GiphyFetch('u5T3TKASE8a0GHUAPgOsc7FPuoGn6iyo');
+const options = { sort: 'recent', limit: 12, rating: 'g' };
 const Game = () => {
   const [input, setInput] = useState('');
   const [searchTerm, setSearchTerm] = useState(null);
-  const handleChange = event => setInput(event.target.value);
-  const handleSubmit = event => {
-    event.preventDefault();
-    setSearchTerm(input)
-  }
-  useEffect(() => {
-    if (searchTerm) {
-      let searchParameters = {
-        'limit': 12,
-        'rating': 'y',
-      };
-      searchParameters['q'] = searchTerm;
-      client.search('gifs', searchParameters)
-      .then(response => {
-        const getUrl = gifObject => gifObject.images.fixed_height_small.url;
-        const links = response.data.map(gifObject => getUrl(gifObject));
-        setCards(makeCards(links));
-        setSearchTerm(null);
-        setInput('');
-      }).catch(err => {
-        console.log(err);
-      })
-    }
-  })
-  const client = GphSdkClient('u5T3TKASE8a0GHUAPgOsc7FPuoGn6iyo');
   const [cards, setCards] = useState(null);
   const [flipCount, setFlipCount] = useState(0);
 
-  if (cards && !cardCount(cards)) {
-    setCards(null);
+  const handleChange = (event) => setInput(event.target.value);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchTerm(input);
+  };
+
+  async function search(searchTerm, options) {
+    try {
+      const result = await gf.search(searchTerm, options);
+      const getURL = (giphObj) => giphObj.images.fixed_height.url;
+      const URLs = result.data.map((resultObj) => getURL(resultObj));
+      setCards(makeCards(URLs));
+    } catch (error) {
+      console.error(`search`, error);
+    }
   }
+
+  useEffect(() => {
+    if (searchTerm) {
+      search(searchTerm, options);
+    }
+  }, [searchTerm]);
 
   useEffect(() => {
     if (flipCount === 2) {
@@ -50,9 +50,14 @@ const Game = () => {
         setCards(nextCards);
       }, 2500);
     }
-  })
+  });
 
-  const clickCard = (key, flipCount) => {
+  if (cards && !cardCount(cards)) {
+    setCards(null);
+    setInput('');
+  }
+
+  function clickCard(key, flipCount) {
     if (shouldFlip(cards, key, flipCount)) {
       setFlipCount(flipCount + 1);
       setCards(flipCard(cards, key));
@@ -67,13 +72,9 @@ const Game = () => {
         value={input}
         cards={cards}
       />
-      <Board
-        cards={cards}
-        flipCount={flipCount}
-        clickCard={clickCard}
-      />
+      <Board cards={cards} flipCount={flipCount} clickCard={clickCard} />
     </>
   );
-}
+};
 
 export default Game;
--- src/Search.jsx
+++ src/Search.jsx
@@ -2,31 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const FormStyled = styled.form`
-
+const Form = styled.form`
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica,
+    Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+  font-size: 1.5rem;
+  margin-top: 3rem;
 `;
 
-export const Search = props => {
+const Label = styled.label``;
+const Input = styled.input`
+  padding: 0.5rem;
+  font-size: 1.5rem;
+  margin-left: 1.5rem;
+`;
+
+export const Search = (props) => {
   const { handleSubmit, handleChange, value, cards } = props;
   if (cards) {
-    return null
+    return null;
   }
   return (
-    <FormStyled onSubmit={handleSubmit}>
-      <label>
-        Search Giphy to start a game:
-        <input type="text" value={value} onChange={handleChange} />
-      </label>
-      <input type="submit" value="Submit" />
-    </FormStyled>
+    <Form onSubmit={handleSubmit}>
+      <Label>
+        Search Giphy to start a game...
+        <Input type="text" value={value} onChange={handleChange} />
+      </Label>
+      <Input type="submit" value="Go!" />
+    </Form>
   );
-}
+};
 
 Search.propTypes = {
   handleChange: PropTypes.func.isRequired,
   handleSubmit: PropTypes.func.isRequired,
   value: PropTypes.string.isRequired,
-  cards: PropTypes.arrayOf(PropTypes.object)
-}
+  cards: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default Search;
